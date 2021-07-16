fd032210b0e3e17b34a30b7fd806599720033f5a
saadrafi <saadrafi81@gmail.com>
2021-07-17 05:07:01 +0600

    test

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'input_screen.dart';
 void main() {
   runApp(MyApp());
 }
-
+// function of adding variable
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
